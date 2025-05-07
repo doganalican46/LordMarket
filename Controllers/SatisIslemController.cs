c59717355fa1661a4ce2c223670c9db360a64b80
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2025-05-07 23:31:35 +0300

    Add project files.

---
+++ Controllers/SatisIslemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LordMarket.Controllers
+{
+    public class SatisIslemController : Controller
+    {
+        // GET: SatisIslem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
