cd76f052735a4e9af30ec5b36b0f11313b82c25b
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2025-05-11 00:26:54 +0300

    satış ekranı tasarımları

---
+++ Controllers/SatisYapController.cs
@@ -0,0 +1,41 @@
+﻿using LordMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LordMarket.Controllers
+{
+    public class SatisYapController : Controller
+    {
+
+        public class SatisIslemViewModel
+        {
+            public List<GelirGider> GelirGider { get; set; }
+            public List<Kategoriler> Kategoriler { get; set; }
+            public List<Musteriler> Musteriler { get; set; }
+            public List<Satislar> Satislar { get; set; }
+            public List<SatisIslem> SatisIslem { get; set; }
+            public List<Urunler> Urunler { get; set; }
+            public List<UserController> UserController { get; set; }
+
+        }
+
+
+        // GET: SatisYap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inde2x()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
