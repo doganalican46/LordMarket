65794e06de5c1508ca88659034f8d84b7c5cf17e
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2025-05-11 12:53:38 +0300

    ana sayfa hızlı ürün eklendi ve yeni ürün ekleme modal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LordMarket.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,87 @@ namespace LordMarket.Controllers
 {
     public class HomeController : Controller
     {
+        LordMarketDBEntities db = new LordMarketDBEntities();
+
+        public class SatisIslemViewModel1
+        {
+            public List<GelirGider> GelirGider { get; set; }
+            public List<Kategoriler> Kategoriler { get; set; }
+            public List<Musteriler> Musteriler { get; set; }
+            public List<Satislar> Satislar { get; set; }
+            public List<SatisIslem> SatisIslem { get; set; }
+            public List<Urunler> Urunler { get; set; }
+
+        }
+
+
+        // GET: SatisYap
         public ActionResult Index()
         {
+            var viewModel = new SatisIslemViewModel1
+            {
+                GelirGider = db.GelirGider.Where(h => h.Status == true).ToList(),
+                Kategoriler = db.Kategoriler.Where(h => h.Status == true).ToList(),
+                Musteriler = db.Musteriler.Where(h => h.Status == true).ToList(),
+                Satislar = db.Satislar.Where(h => h.Status == true).ToList(),
+                SatisIslem = db.SatisIslem.Where(h => h.Status == true).ToList(),
+                Urunler = db.Urunler.Where(h => h.Status == true).ToList()
+            };
+
+            ViewBag.Kategoriler = GetKategoriSelectList();
+
+
+            return View(viewModel); // Eksik olan kısım burasıydı
+        }
+
+
+        private List<SelectListItem> GetKategoriSelectList()
+        {
+            return db.Kategoriler
+                .Where(k => k.Status == true)
+                .Select(k => new SelectListItem
+                {
+                    Text = k.KategoriAd,
+                    Value = k.ID.ToString()
+                }).ToList();
+        }
+
+
+
+
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            ViewBag.Kategoriler = GetKategoriSelectList();
             return View();
         }
 
-      
+        [HttpPost]
+        public ActionResult YeniUrun(Urunler urun)
+        {
+            if (ModelState.IsValid)
+            {
+                urun.Status = true;
+                urun.EklenmeTarihi = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                db.Urunler.Add(urun);
+                db.SaveChanges();
+
+                // Set success message in TempData
+                TempData["SuccessMessage"] = "Ürün başarıyla eklendi!";
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Kategoriler = GetKategoriSelectList();
+            return View(urun);
+        }
+
+
     }
 }
--- Controllers/SatisYapController.cs
+++ Controllers/SatisYapController.cs
@@ -9,6 +9,7 @@ namespace LordMarket.Controllers
 {
     public class SatisYapController : Controller
     {
+        LordMarketDBEntities db = new LordMarketDBEntities();
 
         public class SatisIslemViewModel
         {
@@ -18,7 +19,6 @@ namespace LordMarket.Controllers
             public List<Satislar> Satislar { get; set; }
             public List<SatisIslem> SatisIslem { get; set; }
             public List<Urunler> Urunler { get; set; }
-            public List<UserController> UserController { get; set; }
 
         }
 
@@ -26,9 +26,71 @@ namespace LordMarket.Controllers
         // GET: SatisYap
         public ActionResult Index()
         {
+            var viewModel = new SatisIslemViewModel
+            {
+                GelirGider = db.GelirGider.Where(h => h.Status == true).ToList(),
+                Kategoriler = db.Kategoriler.Where(h => h.Status == true).ToList(),
+                Musteriler = db.Musteriler.Where(h => h.Status == true).ToList(),
+                Satislar = db.Satislar.Where(h => h.Status == true).ToList(),
+                SatisIslem = db.SatisIslem.Where(h => h.Status == true).ToList(),
+                Urunler = db.Urunler.Where(h => h.Status == true).ToList()
+            };
+
+            ViewBag.Kategoriler = GetKategoriSelectList();
+
+
+            return View(viewModel); // Eksik olan kısım burasıydı
+        }
+
+
+        private List<SelectListItem> GetKategoriSelectList()
+        {
+            return db.Kategoriler
+                .Where(k => k.Status == true)
+                .Select(k => new SelectListItem
+                {
+                    Text = k.KategoriAd,
+                    Value = k.ID.ToString()
+                }).ToList();
+        }
+
+
+
+
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            ViewBag.Kategoriler = GetKategoriSelectList();
             return View();
         }
 
-      
+        [HttpPost]
+        public ActionResult YeniUrun(Urunler urun)
+        {
+            if (ModelState.IsValid)
+            {
+                urun.Status = true;
+                urun.EklenmeTarihi = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                db.Urunler.Add(urun);
+                db.SaveChanges();
+
+                // Set success message in TempData
+                TempData["SuccessMessage"] = "Ürün başarıyla eklendi!";
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Kategoriler = GetKategoriSelectList();
+            return View(urun);
+        }
+
+
+
     }
 }
