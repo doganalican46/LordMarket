119458ff923fd0ab08dc87b050d4d4c203129c50
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2025-05-30 18:41:10 +0300

    v.3.0 ana ekrandan borç ödenecek Satış ekranı yenileme süresi 5 dk düşürüldü

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -58,7 +58,8 @@ namespace LordMarket.Controllers
                 Urunler = db.Urunler.Where(h => h.Status == true).ToList(),
                 HizliUrunler = db.Urunler.Where(h => h.Status == true && h.HizliUrunMu == true).ToList(),
                 SonSatis = sonSatis,
-                MusteriAdSoyad = musteriAdSoyad
+                MusteriAdSoyad = musteriAdSoyad,
+
             };
 
             return View(viewModel);
@@ -593,7 +594,52 @@ namespace LordMarket.Controllers
 
 
 
+        [HttpGet]
+        public JsonResult GetMusteriBorc(int id)
+        {
+            var musteri = db.Musteriler.Find(id);
+            if (musteri != null)
+            {
+                return Json(musteri.ToplamBorc, JsonRequestBehavior.AllowGet);
+            }
+            return Json(0, JsonRequestBehavior.AllowGet);
+        }
 
+        [HttpPost]
+        public ActionResult BorcOde(int id, decimal OdenenBorcTutar, string Not, string OdemeTipi)
+        {
+            var musteri = db.Musteriler.Find(id);
+            if (musteri == null) return HttpNotFound();
+
+            if(OdemeTipi== "RaporaYansitma")
+            {
+                musteri.ToplamBorc -= OdenenBorcTutar;
+                string bilgi2 = $"Borç Ödemesi: {DateTime.Now:yyyy-MM-dd HH:mm} - {OdenenBorcTutar} ₺ Not: {Not} Ödeme Tipi: {OdemeTipi} ||";
+                musteri.BosAlan += bilgi2;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            musteri.ToplamBorc -= OdenenBorcTutar;
+
+            string bilgi = $"Borç Ödemesi: {DateTime.Now:yyyy-MM-dd HH:mm} - {OdenenBorcTutar} ₺ Not: {Not} Ödeme Tipi: {OdemeTipi} ||";
+            musteri.BosAlan += bilgi;
+
+            // Yeni SatisIslem kaydı
+            var islem = new SatisIslem
+            {
+                MusteriID = id,
+                ToplamTutar = OdenenBorcTutar,
+                OdemeTipi = OdemeTipi,
+                Tarih = DateTime.Now,
+                UrunListesi = $"Ürün: Veresiye borcu ödendi - Adet: 1 - Tutar: {OdenenBorcTutar} ₺",
+                Status = true
+            };
+            db.SatisIslem.Add(islem);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
 
 
